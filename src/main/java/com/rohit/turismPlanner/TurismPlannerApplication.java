74abd81b74b7b7ed6d426fa5990d9991691db34b
Rohit Singh <rsjat.tr@gmail.com>
2023-09-20 20:22:53 +0530

    my first commit

---
+++ src/main/java/com/rohit/turismPlanner/TurismPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.turismPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohit/turismPlanner/TurismPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.turismPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
